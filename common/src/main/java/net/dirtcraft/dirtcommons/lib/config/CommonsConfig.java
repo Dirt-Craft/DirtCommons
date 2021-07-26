eaaa22510130d1bd4616c1124e58d5b206faf6fa
Shiny <ShinyTourettesX@gmail.com>
2021-07-27 04:35:13 +0800

    clear for 1.12.2 branch

--- common/src/main/java/net/dirtcraft/dirtcommons/lib/config/CommonsConfig.java
+++
@@ -1,9 +0,0 @@
-package net.dirtcraft.dirtcommons.lib.config;
-
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-import org.spongepowered.configurate.objectmapping.meta.Setting;
-
-@ConfigSerializable
-public class CommonsConfig {
-    @Setting("server-name") public static final String SERVER_NAME = "";
-}
